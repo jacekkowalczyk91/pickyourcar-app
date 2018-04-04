998162e81518d54d8dad6e744f6e3ad2591e37da
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-04-05 00:01:57 +0200

    menu fix and css

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import {Navbar, Nav, NavItem} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
+import './Menu.css'
 
 class Menu extends React.Component {
     render() {
         return(
             <div>
-                <Navbar>
+                <Navbar className='nav-menu'>
                     <Nav>
-                        <LinkContainer to="/">
+                        <LinkContainer exact to="/">
                             <NavItem>Lista samochodów</NavItem>
                         </LinkContainer>
-                        <LinkContainer to="/AddCar">
+                        <LinkContainer exact to="/AddCar">
                             <NavItem>Dodaj samochód</NavItem>
                         </LinkContainer>
                     </Nav>
